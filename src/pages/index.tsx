5cefdcf5e57cc85140b6a54b0f15cc057e0caf3e
github keita-0 <laughlaughkaeru@gmail.com>
2023-01-12 23:44:53 +0900

    学習20230112

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { client } from "../libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+type Blog = {
+  title: string;
+  body: string;
+};
+
+const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <p>{`記事の総数：${props.totalCount}件数`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return <li key={content.id}>{content.title}</li>;
+        })}
+      </ul>
+      Hello!
+    </div>
+  );
+};
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.getList({
+    endpoint: "blog",
+  });
+  return {
+    props: { data },
+  };
 };
 
 export default Home;
