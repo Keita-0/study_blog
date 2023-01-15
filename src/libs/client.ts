3374131f967acd4fa59943e7a374502f9d72e854
github keita-0 <laughlaughkaeru@gmail.com>
2023-01-15 13:35:07 +0900

    学習20230115

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "keita-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "CdJAcjfQCzUDcRFUprK5uW0kyVOuVwZKXUD2",
+  apiKey: process.env.API_KEY,
 });
