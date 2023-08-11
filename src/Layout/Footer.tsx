62cab7b6dc660cae6d5d1bf47e7331a8aa969d3d
github keita-0 <laughlaughkaeru@gmail.com>
2023-08-11 23:58:40 +0900

    test

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,8 +1,5 @@
 import type { FC } from "react";
 
-/**
- * @package
- */
 export const Footer: FC = () => {
   return (
     <footer className="flex h-6 w-full items-center justify-center text-sm text-gray-500">
